15a3a422a5b02e43690ce2b671317f867e780509
AlanAcosta460 <115495083+AlanAcosta460@users.noreply.github.com>
2022-10-31 20:03:57 -0600

    Add files via upload

--- C/Practica 7/Ejercicio3P7.c
+++ C/Practica 7/Ejercicio3P7.c
@@ -8,7 +8,9 @@ int main()
     printf("Dame valor\n");
     scanf("%f",&n);
 
+    printf("\n");
     printf("Menu\n");
+    printf("\n");
     printf("Eliga la funcion que quiera aplicar:\n");
     printf("!) Seno\n");
     printf("2) Coseno\n");
@@ -20,48 +22,50 @@ int main()
     printf("8) Logaritmo Natural\n");
     printf("9) Logaritmo en Base 10\n");
     printf("10) Valor Absoluto\n");
+    printf("\n");
     scanf("%i",&op);
-    
+    printf("\n");
+
     switch (op)
     {
     case 1:
-        printf("El Seno de %f es %f:\n",n,sin(n));
+        printf("El Seno de %f es: %f\n",n,sin(n));
     break;
 
     case 2:
-        printf("El Coseno de %f es %f:\n",n,cos(n));
+        printf("El Coseno de %f es: %f\n",n,cos(n));
     break;
 
     case 3:
-        printf("La Tangente de %f es %f:\n",n,tan(n));
+        printf("La Tangente de %f es: %f\n",n,tan(n));
     break;
 
     case 4:
-        printf("El Seno Hiperbolico de %f es %f:\n",n,sinh(n));
+        printf("El Seno Hiperbolico de %f es: %f\n",n,sinh(n));
     break;
 
     case 5:
-        printf("El Coseno Hiperbolico de %f es %f:\n",n,cosh(n));
+        printf("El Coseno Hiperbolico de %f es: %f\n",n,cosh(n));
     break;
 
     case 6:
-        printf("La Tangente Hiperbolica de %f es %f:\n",n,tanh(n));
+        printf("La Tangente Hiperbolica de %f es: %f\n",n,tanh(n));
     break;
 
     case 7:
-        printf("La Raiz Cuadrada de %f es %f:\n",n,sqrt(n));
+        printf("La Raiz Cuadrada de %f es: %f\n",n,sqrt(n));
     break;
 
     case 8:
-        printf("El Logaritmo Natural de %f es %f:\n",n,log(n));
+        printf("El Logaritmo Natural de %f es: %f\n",n,log(n));
     break;
 
     case 9:
-        printf("El Logaritmo Base 10 de %f es %f:\n",n,log10(n));
+        printf("El Logaritmo Base 10 de %f es: %f\n",n,log10(n));
     break;
 
     case 10:
-        printf("El Valor Absoluto de %f es %f:\n",n,fabs(n));
+        printf("El Valor Absoluto de %f es: %f\n",n,fabs(n));
     break;
 
     default:
